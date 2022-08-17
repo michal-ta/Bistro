67895c7cae53431966f82dd88d6b1b70610461bc
Michal Takáč <michaltakac93@gmail.com>
2022-08-17 19:30:36 +0200

    adding modal after start of page with information

---
+++ app.js
@@ -0,0 +1,12 @@
+let ModalCloseBtn=document.querySelector(".modal_close");
+let modal=document.querySelector(".modal");
+
+
+
+window.addEventListener("load" ,function() {
+    modal.classList.add("active");
+});
+
+ModalCloseBtn.addEventListener("click", function(){
+    modal.classList.remove("active");
+})
